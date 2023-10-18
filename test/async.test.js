b35aef1c89254ca66aca9301cdd62b07d7cacbf9
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-10-18 10:23:24 +0700

    Node Js - Unit test - Async Matcher

--- test/async.test.js
+++ test/async.test.js
@@ -4,3 +4,8 @@ test("Test async function ", async () => {
   const result = await sayHello("Aziz");
   expect(result).toBe("Hello Aziz");
 });
+
+test("Test async matcher function ", async () => {
+  await expect(sayHello("Aziz")).resolves.toBe("Hello Aziz");
+  await expect(sayHello()).rejects.toBe("Name is empty");
+});
